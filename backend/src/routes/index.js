2eb411c1d58f6561d65cc987d1f538e279c3298a
hdgomez8 <hdgomez8@hotmail.com>
2023-08-30 05:31:44 -0500

    Controller/Handler Subcategories

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,7 @@
 const { Router } = require("express");
 const products = require("../routes/products/routeProducts")
 const categories = require ("../routes/categories/routeCategories")
+const subCategories = require ("../routes/subCategories/routeSubCategories")
 
 const router = Router();
 
@@ -9,6 +10,9 @@ router.use("/products", products)
 
 //Ruta para acceder a las categorias
 router.use("/categories", categories)
+
+//Ruta para acceder a las subcategorias
+router.use('/subcategoria',subCategories)
 
 
 module.exports = router;
