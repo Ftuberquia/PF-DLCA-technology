9a39670b38ab2ffd8481695c17dac45a22da671e
Orliluq <122529721+Orliluq@users.noreply.github.com>
2023-08-31 01:49:36 -0400

    Update routeProducts.js

--- backend/src/routes/products/routeProducts.js
+++ backend/src/routes/products/routeProducts.js
@@ -1,13 +1,10 @@
 const {Router} = require ("express")
 const { getAllProductsHandler } = require("../../handlers/getAllProductsHandler");
-<<<<<<< HEAD
 const { getDetailHandler } = require("../../handlers/getProductDetailHandler");
 const getDbProducts = require("../../controllers/getDbProducts");
 const postCreateProduct = require("../../controllers/postCreateProduct");
-=======
 const { getDetailHandler } = require("../../handlers/products/getProductDetailHandler");
 const postProduct = require ("../../handlers/products/createProductHandler") 
->>>>>>> 51ffac2285cccea75b914f6b0838b123041c5366
 
 const products = Router()
 
@@ -16,10 +13,9 @@ products.get("/", getAllProductsHandler)
 //este trae los detalles del producto
 products.get("/:id", getDetailHandler)
 
-<<<<<<< HEAD
 products.post("/", postCreateProduct)
-=======
+
 products.post("/", postProduct)
->>>>>>> 51ffac2285cccea75b914f6b0838b123041c5366
+
 
 module.exports = products
