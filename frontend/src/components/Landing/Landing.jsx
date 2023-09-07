1c25fed296dd5c52a6a08a7fed60c1fe50e7a9a8
orliluq <orlibet@gmail.com>
2023-09-07 01:13:01 -0400

    landing terminada

--- frontend/src/components/Landing/Landing.jsx
+++ frontend/src/components/Landing/Landing.jsx
@@ -49,20 +49,30 @@ function Landing() {
             />
           ))}
         </div> */}
+        <div>
+        <div className='globo'>
+          <img
+            className='imgPres'
+            src="https://i.ibb.co/GHM1qNZ/clientes-felices.jpg"
+            alt=""
+          />
+          <p className='texto'>
+            Somos una empresa comprometida con la calidad y excelencia en la
+            industria de la tecnología. Ofrecemos una amplia gama de productos
+            de alta calidad y a los mejores precios. Contamos con un equipo de 
+            profesionales capacitados en brindar un servicio excepcional de 
+            atención a nuestros clientes y proveedores. En DLCA Technology, nos
+            esforzamos por ser líderes en la industria y proporcionar soluciones
+            confiables y eficientes para todos nuestros clientes.
+          </p>
+        </div>
+        </div>
         <div className="pcgamer">
           <Link to="/products">
             <img src="https://i.ibb.co/Xprs2Rs/pc-gamer2.jpg" alt="" />
           </Link>
         </div>
-        <div className="rews">
-          <Link to="/reviews">
-            {/* <img
-              src="https://i.ibb.co/Xprs2Rs/pc-gamer2.jpg"
-              alt=""
-            /> */}
-          </Link>
-
-        </div>
+        
         <Questions />
         <section className="banners">
           <div className="bannerOne">
