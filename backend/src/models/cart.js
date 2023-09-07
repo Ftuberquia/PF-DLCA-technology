ece74cfa002b7dcbcc57425718f74926570dfdc2
hdgomez8 <hdgomez8@hotmail.com>
2023-09-07 06:47:03 -0500

    cart

--- backend/src/models/cart.js
+++ backend/src/models/cart.js
@@ -1,26 +1,47 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
 const Cart = (sequelize) => {
-    sequelize.define("cart", {
+  sequelize.define(
+    "cart",
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      productId: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      quantity: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      pagado: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false,
+      },
+      fechaCompra: {
+        type: DataTypes.STRING,
+        allowNull: true, // Permitir valores nulos
+        defaultValue: null, // Valor por defecto
+      },
+    },
+    {
+      tableName: "cart",
+      timestamps: false,
+      freezeTableName: true,
+    }
+  );
 
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true,
-            allowNull: false
-        },
-        productId: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        },
-        quantity: {
-            type: DataTypes.INTEGER,
-            allowNull: false
-        }
-    }, { 
-        tableName: 'cart',
-        timestamps: false,
-        freezeTableName: true, });
-};   
+  // Definir las relaciones con otros modelos
+  Cart.associate = (models) => {
+    Cart.belongsTo(models.Users, {
+      foreignKey: "usersId",
+    });
+  };
+};
 
 module.exports = Cart;
