b41c0433ae4ec058a9d76a8756bae464352bfb22
orliluq <orlibet@gmail.com>
2023-09-07 18:03:07 -0400

    estilos y responsive

--- frontend/src/views/Featured/Featured.jsx
+++ frontend/src/views/Featured/Featured.jsx
@@ -1,49 +1,46 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import "./Featured.css";
+import style from "./Featured.module.css";
 
 function Featured() {
   return (
-    <div className="contAccesorios">
-      <div className="containerAcc mt-100">
-        <div className="titulo">
+    <div className={style.contAccesorios}>
+      <div className={style.containerAccmt100}>
+        <div className={style.titulo}>
           <h2>
-            <span className="spanTitleAcc1">💚</span> ¡DESTACADOS
-            <span className="spanTitleAcc"> GAMER</span>!
+            <span className={style.spanTitleAcc1}>💚</span> ¡DESTACADOS
+            <span className={style.spanTitleAcc}> GAMER</span>!
           </h2>
         </div>
-        <div className="card-container">
-          <div className="card mb-30">
-            <NavLink to="/productos">
+        <div className={style.cardcontainer}>
+          <div className={style.cardmb30}>
+            <NavLink to="/product/22">
               <img
                 src="https://i.ibb.co/zxp1fjm/img23.png"
                 alt="Notebooks y Laptops"
               />
-              </NavLink>
-            </div>
-            <div className="card mb-30">
-            <NavLink to="/productos">
+            </NavLink>
+          </div>
+          <div className={style.cardmb30}>
+            <NavLink to="/product/36">
               <img
                 src="https://i.ibb.co/DtxVgrx/img299.png"
                 alt="Periféricos"
               />
-              </NavLink>
-            </div>
-            <div className="card mb-30">
-            <NavLink to="/productos">
+            </NavLink>
+          </div>
+          <div className={style.cardmb30}>
+            <NavLink to="/product/42">
               <img
                 src="https://i.ibb.co/djrfxBd/img69.png"
                 alt="Componentes PC"
               />
-              </NavLink>
-            </div>
-            <div className="card mb-30">
-            <NavLink to="/productos">
-              <img
-                src="https://i.ibb.co/hRSLRDX/img273.jpg"
-                alt="Accesorios"
-              />
-            </NavLink >
+            </NavLink>
+          </div>
+          <div className={style.cardmb30}>
+            <NavLink to="/product/96">
+              <img src="https://i.ibb.co/hRSLRDX/img273.jpg" alt="Accesorios" />
+            </NavLink>
           </div>
         </div>
       </div>
