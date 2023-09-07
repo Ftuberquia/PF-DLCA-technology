e208a25955839b8915757c127a70e3ef9dc250c9
Ftuberquia <frank.tuberquiarojas@gmail.com>
2023-09-06 21:23:33 -0500

    agregamos build, para deploy front

--- frontend/src/views/Stripe/Stripe.jsx
+++ frontend/src/views/Stripe/Stripe.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import style from './Stripe.module.css';
 import { loadStripe } from "@stripe/stripe-js";
 import  { Elements, CardElement, useStripe, useElements } from "@stripe/react-stripe-js"
+import Productos from "../Productos/Productos";
 
 const stripePromise = loadStripe("pk_test_51NnMQaEUVHui4qp0KnEfLflyUrkDfZDN9jLhIq7Vzb4RGVvCG0tCfEDmgi9GKV1CYCXc5TYzU7FcS4BXCXmSv8tC00L9f6qNwM")
 
@@ -15,14 +17,22 @@ const CheckoutForm = () => {
         const {error, paymentMethod } = await stripe.createPaymentMethod({
             type: 'card',
             card: elements.getElement(CardElement)
-        })
+        });
+
+        if(!error){
+            console.log(paymentMethod);
+        }
 
     }
 
     return (
-    <form onSubmit={handleSubmit}>
-        <CardElement/>
-        <button>
+    <form onSubmit={handleSubmit}className="">
+
+        <img className={style.products} src={''} alt="product-cart"/>
+        <div className={style.cardContainer}>
+            <CardElement className={style.visa}/>
+        </div>
+        <button className={style.button}>
             Comprar
         </button>
     </form>)
@@ -30,12 +40,15 @@ const CheckoutForm = () => {
 
 const Stripe = () => {
     return (
-        <div>
-            <h1>Pasarela de pagos Stripe</h1>
-        <Elements stripe={stripePromise}>        
-            <CheckoutForm/>
+        <main className={style.card}>
+            <Elements stripe={stripePromise}>        
+            <div className={style.container}>
+                <h1>Pasarela de pagos Stripe</h1>
+                <CheckoutForm/>
+            </div>
         </Elements>
-        </div>
+        </main>
+        
         
     );
 };
