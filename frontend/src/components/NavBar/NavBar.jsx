80c33171dea10636ccf44d53c705e97ed87b94f4
orliluq <orlibet@gmail.com>
2023-09-07 17:24:32 -0400

    responsive

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -1,4 +1,4 @@
-import styles from "./NavBar.module.css";
+import style from "./NavBar.module.css";
 import { Link } from "react-router-dom";
 // import logo from "../../img/logo-dlca.png";
 import personIcon from "../../img/person.svg";
@@ -10,38 +10,38 @@ const NavBar = () => {
   const { cart } = useSelector((state) => state?.cart || {});
 
   return (
-    <nav className={styles.navbar}>
+    <nav className={style.navbar}>
       <Link to={"/"}>
         {/* <img src={logo} className={styles.logo} alt="Logo" /> */}
       </Link>
       <span>
-        <Link className={styles.name} to={"/"}>
+        <Link className={style.name} to={"/"}>
           DLCA TECHNOLOGY
         </Link>
       </span>
       <span>
-        <Link className={styles.links} to={"/productos"}>
+        <Link className={style.links} to={"/productos"}>
           Productos
         </Link>
       </span>
       <span>
-        <Link to={"/about"} className={styles.links}>
+        <Link to={"/about"} className={style.links}>
           Sobre nosotros
         </Link>
       </span>
       <span>
-        <Link to={"/contacto"} className={styles.links}>
+        <Link to={"/contacto"} className={style.links}>
           Contáctenos
         </Link>
       </span>
-      <div className={styles.buttons}>
+      <div className={style.buttons}>
         <Link to={"/user"}>
           <img src={personIcon} alt="Person" />
         </Link>
         <Link to={"/favorites"}>
           <img src={heartIcon} alt="Favorites" />
         </Link>
-        <Link to={"/cart"} className={styles.cart}>
+        <Link to={"/cart"} className={style.cart}>
           <img src={shoppingCartIcon} alt="Shopping Cart" />
           <span>{cart?.total_items ? cart?.total_items : "0"}</span>
         </Link>
@@ -51,3 +51,4 @@ const NavBar = () => {
 };
 
 export default NavBar;
+
