817f24a7d9217a2bd8183c0c81b313cbd71e07c5
CarlosMayaDev <carlosdavidmaya1@gmail.com>
2023-09-08 11:10:27 -0500

    backend de reviews, aun desarrollando

--- backend/src/controllers/reviews/postReview.js
+++ backend/src/controllers/reviews/postReview.js
@@ -1,12 +1,14 @@
 const { UserProductReviews } = require("../../db")
 
 const postReviews = async(productId, comment, rating, userId) =>{
-    if(!rating && !comment){
+    if(!rating || !comment || !userId){
         throw Error('Todos los campos son obligatorios')
     }
     const newReview = await UserProductReviews.create({
+        productId,
         comment,
         rating,
+        userId: parseInt(userId)
     })
     return newReview
 }
