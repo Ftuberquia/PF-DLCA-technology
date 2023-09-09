54a21ef62c43a022abfa2a370e5e3a912b07248d
AFLopezLagomarsino <andresinfernoxii@gmail.com>
2023-09-08 22:52:11 -0300

    producs y cards actualizados, ahora el paginado se realiza en el front, es un scroll infinito

--- frontend/src/views/Productos/Productos.jsx
+++ frontend/src/views/Productos/Productos.jsx
@@ -1,26 +1,46 @@
 import React, { useEffect, useState } from "react";
 import Cards from "../../components/Cards/Cards";
 import { useDispatch, useSelector } from "react-redux";
-import { getAllProducts } from "../../redux/actions/index";
+
 import Filters from "../../components/Filters/Filters";
 import { addToCart } from "../../redux/actions/index";
-
+import axios from "axios";
 import style from './Productos.module.css'
 
 function Productos() {
   const dispatch = useDispatch();
-  const productsData = useSelector((state) => state.products);
+
   const cart = useSelector((state) => state.cart);
 
+  const [productsData, setProducts] = useState([])
+  const [page, setPage] = useState(1)
+
   useEffect(() => {
-    dispatch(getAllProducts());
-  }, [dispatch]);
+    loadProducts()
+  }, [page]);
+
+  const loadProducts = async () =>{
+    try {
+      const response = await axios.get(`/products?page=${page}`)
+      const data=await response.data;
+      setProducts((prevProducts)=>[...prevProducts, ...data])
+    } catch (error) {
+      console.error('Error en el front',error);
+    }
+  }
+
+  const handleScroll=(e)=>{
+    const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
+    if (scrollHeight - scrollTop === clientHeight) {
+      setPage((prevPage) => prevPage + 1); // Incrementar el número de página cuando se llega al final del contenedor
+    }
+  }
 
   return (
     <div className={style.homeContainer}>
       <Filters />
       <div>
-        <Cards products={productsData} />
+        <Cards products={productsData} scroll={(e)=>handleScroll(e)}/>
       </div>
     </div>
   );
