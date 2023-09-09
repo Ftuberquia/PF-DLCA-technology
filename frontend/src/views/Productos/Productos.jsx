b4223f44195783f3fea692ab2ee3b015d84786c2
agusv <agusvarela5@gmail.com>
2023-09-09 06:18:35 -0300

    Se incorporo la solicitud de los productos desde el back, tambien se realizo la implementacion del filtrado desde la misma solicitud, y el paginado.

--- frontend/src/views/Productos/Productos.jsx
+++ frontend/src/views/Productos/Productos.jsx
@@ -12,35 +12,122 @@ function Productos() {
 
   const cart = useSelector((state) => state.cart);
 
-  const [productsData, setProducts] = useState([])
+  const [productsData, setProductsData] = useState([])
+  const [filteredProducts,setFilteredProducts]=useState([])
   const [page, setPage] = useState(1)
+  const [isLastPage, setIsLastPage] = useState(false);
 
   useEffect(() => {
+    setProductsData([])
     loadProducts()
-  }, [page]);
+  }, [page, filteredProducts]);
 
-  const loadProducts = async () =>{
+  async function handlePrevPage(e){
+    if(page===1||page<1){
+      setPage(1)
+    }else{
+      setPage(page-1)
+      setIsLastPage(false)
+    }
+  }
+
+  const handleNextPage = async (e)=>{
+    let url=`/products?page=${page+1}`
+
+      if (filteredProducts.brand) {
+        url += `&brand=${filteredProducts.brand}`;
+      }
+
+      // Verificar y asignar el valor de subcategory
+      if (filteredProducts.subcategory) {
+        url += `&subcategory=${filteredProducts.subcategory}`;
+      }
+  
+      // Verificar y asignar el valor de category
+      if (filteredProducts.category) {
+        url += `&category=${filteredProducts.category}`;
+      }
+  
+      // Verificar y asignar el valor de order
+      if (filteredProducts.order) {
+        url += `&order=${filteredProducts.order}`;
+      }
+  
+      // Verificar y asignar el valor de priceOrder
+      if (filteredProducts.priceOrder) {
+        url += `&priceOrder=${filteredProducts.priceOrder}`;
+      }
+      
+      const response = await axios.get(url);
+      const data = await response.data;
+    
+    if(data.length!==0){
+      setPage(page+1)
+      setIsLastPage(false);
+    } else {
+      setIsLastPage(true);
+    }
+
+  }
+
+  const loadProducts = async () => {
     try {
-      const response = await axios.get(`/products?page=${page}`)
-      const data=await response.data;
-      setProducts((prevProducts)=>[...prevProducts, ...data])
-    } catch (error) {
-      console.error('Error en el front',error);
-    }
-  }
+      let url=`/products?page=${page}`
 
-  const handleScroll=(e)=>{
-    const { scrollTop, clientHeight, scrollHeight } = e.currentTarget;
-    if (scrollHeight - scrollTop === clientHeight) {
-      setPage((prevPage) => prevPage + 1); // Incrementar el número de página cuando se llega al final del contenedor
+      if(filteredProducts.length!==0){
+
+        if (filteredProducts.brand) {
+          url += `&brand=${filteredProducts.brand}`;
+        }
+
+        // Verificar y asignar el valor de subcategory
+        if (filteredProducts.subcategory) {
+          url += `&subcategory=${filteredProducts.subcategory}`;
+        }
+    
+        // Verificar y asignar el valor de category
+        if (filteredProducts.category) {
+          url += `&category=${filteredProducts.category}`;
+        }
+    
+        // Verificar y asignar el valor de order
+        if (filteredProducts.order) {
+          url += `&order=${filteredProducts.order}`;
+        }
+    
+        // Verificar y asignar el valor de priceOrder
+        if (filteredProducts.priceOrder) {
+          url += `&priceOrder=${filteredProducts.priceOrder}`;
+        }
+        
+        const response = await axios.get(url);
+        const data = await response.data;
+        setProductsData([...data]);
+
+      }else{
+        const response = await axios.get(url);
+        const data = await response.data;
+        setProductsData(() => [...data]);
+      }
+    } catch (error) {
+      console.error("Error en el front", error);
     }
-  }
+  };
+
+  const updateFilters = (fieldName, selectedValue) => {
+    setFilteredProducts((prevFilters) => ({
+      ...prevFilters,
+      [fieldName]: selectedValue,
+    }));
+    setPage(1)
+    loadProducts();
+  };
 
   return (
     <div className={style.homeContainer}>
-      <Filters />
+      <Filters updateFilters={updateFilters}/>
       <div>
-        <Cards products={productsData} scroll={(e)=>handleScroll(e)}/>
+      <Cards products={filteredProducts.length > 0 ? filteredProducts : productsData} page={page} isLastPage={isLastPage} handleNextPage={handleNextPage} handlePrevPage={handlePrevPage}/>
       </div>
     </div>
   );
