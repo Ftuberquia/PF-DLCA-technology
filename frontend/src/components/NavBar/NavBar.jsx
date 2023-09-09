345af161890f7d157d8939e0aedb17295f307397
CarlosMayaDev <carlosdavidmaya1@gmail.com>
2023-09-09 12:27:00 -0500

    modificacion navbar, creacion UserProfileView, autentica con auth0, crea usuario en BD cuando ingresa por primera vez, UserProfileView quedaria como pagina principal del usuario por primera vez

--- frontend/src/components/NavBar/NavBar.jsx
+++ frontend/src/components/NavBar/NavBar.jsx
@@ -5,13 +5,41 @@ import heartIcon from "../../img/heart.svg";
 import shoppingCartIcon from "../../img/shopping-cart.svg";
 import { useSelector } from "react-redux";
 import { useAuth0 } from "@auth0/auth0-react";
-import { useState } from "react";
+import { useEffect } from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import style from "./NavBar.module.css";
+import axios from "axios";
 
 const NavBar = () => {
   const { cart } = useSelector((state) => state?.cart || {});
-  const { loginWithPopup} = useAuth0();
+  const { loginWithPopup, isAuthenticated, user} = useAuth0();
+
+  useEffect(() => {
+    // Cuando el usuario esté autenticado, envía los datos al servidor
+    if (isAuthenticated && user) {
+      const userData = {
+        first_name: user.given_name,
+        last_name: user.family_name,
+        username: user.nickname,
+        email: user.email,
+        // Otros campos de datos que quieras enviar
+      };
+
+      // Realiza la solicitud al servidor para guardar los datos del usuario
+      axios.post("http://localhost:3001/users/", userData)
+        .then((response) => {
+          if (response.status === 200) {
+            console.log("Usuario creado con éxito en el servidor");
+            // Realizar acciones adicionales si es necesario
+          } else {
+            console.error("Error al crear el usuario en el servidor");
+          }
+        })
+        .catch((error) => {
+          console.error("Error al realizar la solicitud al servidor:", error);
+        });
+    }
+  }, [isAuthenticated, user]);
 
   return (
     <nav className={style.navbar}>
