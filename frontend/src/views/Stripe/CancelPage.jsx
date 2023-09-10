3d3e3b8f31440c2fb1352b34fdd68fa0c3b975e7
Ftuberquia <frank.tuberquiarojas@gmail.com>
2023-09-10 08:04:48 -0500

    arreglo conflictos

--- frontend/src/views/Stripe/CancelPage.jsx
+++ frontend/src/views/Stripe/CancelPage.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 import styles from "./CancelPage.module.css"; // Importa los estilos
 
 const CancelPage = () => {
@@ -7,7 +6,7 @@ const CancelPage = () => {
   return (
     <div className={styles.contenedorCancel}>
     <div className={styles["cancel-container"]}>
-      <h1 className={styles["cancel-title"]}>Compra Cancelada</h1>
+      <h1 className={styles["cancel-title"]}>Compra Rechazada</h1>
       <h1 className={styles["cancel-mss"]}>Tu compra fue rechazada por falla de Credenciales</h1>
       <a href="/productos" className={styles["back-link"]}>
         Volver a Productos
