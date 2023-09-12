e43a91ebb4dfeaccc0274ad7a24fa1f6bc3c7318
CarlosMayaDev <carlosdavidmaya1@gmail.com>
2023-09-12 09:30:59 -0500

    creacion findOrCreateUserController

---
+++ backend/src/controllers/users/findOrCreateUserController.js
@@ -0,0 +1,33 @@
+const { Users } = require("../../db")
+
+const findOrCreateUserController = async (userData) => {
+
+    try {
+        const { id, first_name, last_name, username, email } = userData;
+    
+        // Utiliza findOrCreate para buscar o crear un usuario
+        const [user, created] = await Users.findOrCreate({
+          where: { id }, // Busca por el campo 'id'
+          defaults: {
+            first_name,
+            last_name,
+            username,
+            email,
+            // Otros campos de datos que quieras asignar por defecto
+          },
+        });
+    
+        if (created) {
+          // El usuario fue creado
+          return { status: 201, data: { message: 'Usuario creado con éxito', user } };
+        } else {
+          // El usuario ya existía
+          return { status: 200, data: { message: 'Usuario encontrado', user } };
+        }
+      } catch (error) {
+        console.error('Error al crear o buscar un usuario:', error);
+        throw error;
+      }
+};
+
+module.exports = findOrCreateUserController;
