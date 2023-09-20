e61fc85caf6fb00468d4370561bd8aeb574e5054
kayitaC2024 <kayita_y@hotmail.com>
2023-09-20 09:02:31 -0500

    css qr

--- frontend/src/views/QRCodeGenerator/QRCodeGenerator.jsx
+++ frontend/src/views/QRCodeGenerator/QRCodeGenerator.jsx
@@ -4,9 +4,10 @@ import QRCode from "qrcode.react";
 import QRCodeLink from "qrcode";
 
 import style from "./QRCodeGenerator.module.css";
+import imagen from "./imagen/qrcode.png"
 
- //https://pf-dlca-technology-3davhwpi3-ftuberquia.vercel.app/productos
-//"https://www.instagram.com/dlcatecnology/?igshid=MzRlODBiNWFlZA%3D%3D"
+ // https://pf-dlca-technology-3davhwpi3-ftuberquia.vercel.app/productos
+//  https://www.instagram.com/dlcatecnology/?igshid=MzRlODBiNWFlZA%3D%3D
   
 
 function  QRCodeGenerator() {
@@ -30,22 +31,28 @@ function handleGenerate(link_url){
 };
 
   return (
-    <div className={style.container}>
+    <div className={`${style.container}`}>
      
-      <QRCode 
+      <QRCode
+       className={`${style.code}`} // Aplica la clase CSS al QR Code
        value={link}
 
       />
 
       <input
-      className={style.input}
+      className={`${style.input}`}
       placeholder="Digite su Link..."
       value={link}
       onChange={ (event) => handleQrcode(event)} // Escucha cambios en el input
 
       />
       
-      <a href={qrcodeLink} download={`qrcode.png`}>Bajar QrCode</a>
+      <a href={qrcodeLink} download={`qrcode.png`} className={style.link}>Bajar QrCode</a>
+
+      <div className={`${style.img}`}>
+      <img src={imagen} alt="qrcode" />
+
+      </div>
 
     </div>
   );
